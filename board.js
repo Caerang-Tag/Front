0e88ca82e7e0c326f17f9b9759ec435f22918d3d
myth <okmlnsunok@naver.com>
2019-09-28 12:29:53 +0900

    토글구현

---
+++ board.js
@@ -0,0 +1,13 @@
+/**검색기간 toggle*/
+
+function showID(con){
+    obj = document.getElementById(con);
+
+    if(obj.style.display == "none")
+        obj.style.display="inline";
+    else
+        obj.style.display="none";
+}
+
+
+
