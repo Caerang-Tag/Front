bdd3ae36bf540e877fb914e4782ac97786ab4ff0
myth <okmlnsunok@naver.com>
2019-10-30 14:13:05 +0900

    게시판수정

--- board.js
+++ board.js
@@ -1,17 +1,4 @@
-/**검색기간 toggle*/
 
-function showID(con, I){
-    obj = document.getElementById(con);
-    img = document.getElementById(I);
-    if(obj.style.display == "none"){
-        obj.style.display="inline";
-        img.src = img.src.replace("picture/Reverse_Triangle.png", "picture/Triangle.png");
-}
-    else{
-        obj.style.display="none";
-        img.src = img.src.replace("picture/Triangle.png", "picture/Reverse_Triangle.png");
-    }
-}
 
 
 
